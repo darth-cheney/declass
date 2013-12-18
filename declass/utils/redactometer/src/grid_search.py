2481ea4e95f3f3ac4dda1394181f12ed11fc1d75
Sophie Chou <sbchou@gmail.com>
2013-12-18 10:47:39 -0500

    update grid train

--- declass/utils/redactometer/src/grid_search.py
+++ declass/utils/redactometer/src/grid_search.py
@@ -42,18 +42,9 @@ def complete_search(training, img_dir, metric, min_start, min_stop, max_start, m
     max_range = np.arange(max_start, max_stop, step)
     param_grid = {'min_width_ratio' : min_range,
                 'max_width_ratio': max_range,
-                'min_height_ratio': min_range,
+                'min_height_ratio': mirn_range,
                 'max_height_ratio': max_range}
     return gridsearch(param_grid, training, img_dir, metric)
-
-def eval(test, params, img_root, metric, censor_type="dark"):
-    """Evaluation results on test set"""
-
-    if metric == "is_censored":
-        return boolean_scoring(test, img_root, params)
-
-    if metric == "total_censor":
-        return count_scoring(test, img_root, params)
 
 def boolean_scoring(data, url, params):
     """does not filter by censor type yet"""
@@ -62,16 +53,64 @@ def boolean_scoring(data, url, params):
     score = float(correct)/float(len(data.index))
     return score
 
-def count_scoring(data, url, params):
-    """does not filter by censor type yet"""
-    correct = sum(len(redactometer.censor_dark(url + i, **params)[1]) \
-                == data.ix[i]['total_censor'] for i in data.index)
-    imgs = [(i, len(redactometer.censor_dark(url + i, **params)[1]), data.ix[i]['total_censor']) for i in data.index if len(redactometer.censor_dark(url + i, **params)[1]) \
-                == data.ix[i]['total_censor']]
-    
+def count_scoring_binary(data, url, params):
+    """Returns correct only if entire document correct"""
+    correct = sum(len(redactometer.censor_fill(url + i, **params)[1]) \
+                == data.total_censor[i] for i in data.index)
+    results = [(i, len(redactometer.censor_fill(url + i, **params)[1]), data.total_censor[i]) \
+                    for i in data.index]
+
     score = float(correct)/float(len(data.index))
-    #if correct > 10:
-    #    print params, correct, score
+    return score, results
+ 
+
+def count_scoring(data, url, params):
+    """Count number of censors detected over real number""" 
+    error = np.mean([abs_error(len(redactometer.censor_fill(url + i, **params)[1]), \
+              data.total_censor[i]) for i in data.index])
+    return 1 - error
+
+
+def blurred_count_scoring(data, url, aperture):
+    """Count number of censors detected over real number, aperture default 25""" 
+    error = np.mean([abs_error(len(redactometer.blurred_detection(url + i, aperture)[0]), \
+              data.total_censor[i]) for i in data.index])
+    return 1 - error
+
+
+def f_score(data, url, params):
+    predicted = float(sum([len(redactometer.censor_fill(url + i, **params)[1]) \
+                    for i in data.index]))
+    truepos = float(sum([len(redactometer.censor_fill(url + i, **params)[1]) \
+                 == data.total_censor[i] for i in data.index]))
+    #verify by coordinate! Above not correct. Truepos = relevant ^ retrieved
+
+
+    precision = truepos / predicted
+    recall = truepos / sum(data.total_censor)
+
+    F = 2 * (precision * recall) / (precision + recall)
+
+    return F
+
+def eval(data, url, params):
+    """ return score and individual metrics"""
+    score = count_scoring(data, url, params)
     
-    return score, correct, imgs
+    metrics = [(i, len(redactometer.censor_fill(url + i, **params)[1]),  data.total_censor[i], \
+                    abs_error(len(redactometer.censor_fill(url + i, **params)[1]), \
+                    data['total_censor'][i])) for i in data.index]
+        
+    metrics = pandas.DataFrame(metrics, columns=['img', 'observed', 'actual', 'error'])
+    #scores.to_csv(outfile, sep="\t")
+
+    return score, metrics
+
+
+def abs_error(observed, actual):
+    return abs(float(actual - observed) / float(actual))
+
+
+
+
 
