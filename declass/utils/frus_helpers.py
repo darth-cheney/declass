80ac95fb6afe7c4f1d38dfcb304fe12ae33bdfe0
dkrasner <dkrasner@math.columbia.edu>
2014-04-25 12:47:00 -0400

    finished up epub frus parsers

--- declass/utils/frus_helpers.py
+++ declass/utils/frus_helpers.py
@@ -1,8 +1,11 @@
 ##### various util helpers to deal with frus collection
 import re
 import dateutil.parser as dparser
+import bs4
+import BeautifulSoup as bs1
+import pandas as pd
+import numpy as np
 
-from bs4 import BeautifulSoup
 from unidecode import unidecode
 from collections import defaultdict
 
@@ -13,6 +16,32 @@ from collections import defaultdict
 #unzip it into a nice directory srtructure and parse the individual fieles with the 
 #functions below
 
+def parse_epub_book(base_dir, vol_name):
+    ###get all the nec paths
+    vol_contents_dir = os.path.join(base_dir, vol_name, 'OEBPS')
+    vol_contents = os.listdir(vol_contents_dir)
+    persons = os.path.join(vol_contents_dir, 'persons.html')
+    preface = os.path.join(vol_contents_dir, 'preface.html')
+    title = os.path.join(vol_contents_dir, 'title.html')
+    terms = os.path.join(vol_contents_dir, 'terms.html')
+    refs = [os.path.join(vol_contents_dir,f) 
+            for f in vol_contents if 
+            re.search('(comp\d{,3}|ch\d{,3})\.html', f)]
+    docs = [os.path.join(vol_contents_dir,f)
+            for f in vol_contents if re.search('d\d{,4}\.html', f)]
+
+    parsed_persons = parse_html_persons(persons)
+    parsed_preface = parse_html_preface(preface)
+    parsed_title = parse_html_title(title)
+    parsed_terms = parse_html_terms(terms)
+    parsed_refs = parse_html_refs(refs, id_prefix=vol_name)
+    parsed_docs = parse_html_docs(docs, parsed_persons.index, 
+            id_prefix=vol_name)
+    return {'persons': parsed_persons, 'preface': parsed_preface, 
+            'title': parsed_title, 'terms': parsed_terms, 'refs': parsed_refs,
+            'docs': parsed_docs}
+
+
 def parse_html_persons(html_file):
     """
     Parses persons.html from epub.
@@ -20,29 +49,41 @@ def parse_html_persons(html_file):
     Parameters
     ----------
     html_file : open file obj
+
+    Returns
+    -------
+    pd.DataFrame 
     """
-    names_list = []
-    html = html_file.read()
+    names = pd.DataFrame(columns=['first', 'last', 'suffix'])
+    html = open(html_file).read()
     html = html.decode('ascii', 'ignore')
-    soup = BeautifulSoup(html)
+    soup = bs4.BeautifulSoup(html)
     lines = soup.findAll('li')
     for line in lines:
         name_tag = line.findAll('span', 'tei-persName')
         if len(name_tag) is not 1: continue
-        last_name, first_name = name_tag[0].getText().split(',')[:2]
+        try:
+            last_name, first_name = name_tag[0].getText().split(',')[:2]
+        except ValueError:
+            last_name = name_tag[0].getText().strip()
+            first_name = ''
+        name_id = re.sub(r'\s+', '_',last_name+' '+first_name)
         try:
             suffix = ','.join(name_tag[0].getText().split(',')[2:])
         except IndexError:
             suffix = None
         last_name = str(last_name.strip())
         first_name = str(first_name.strip())
-        description = re.findall(r'</strong>(.*)</li>$', str(line))[0].strip()
-        names_list.append(
-                {'last': last_name, 'first': first_name, 
-                    'suffix': suffix, 'description': description})
-    return names_list
-
-
+        description = re.findall(r'</strong>(.*)</li>$', str(line))
+        if description: 
+            description = description[0].strip()
+        else:
+            description = None
+        names = names.append(
+                pd.DataFrame({'last': last_name, 'first': first_name, 
+                    'suffix': suffix, 'description': description},
+                    index=[name_id]))
+    return names
     
 def parse_html_preface(html_file):
     """
@@ -52,11 +93,10 @@ def parse_html_preface(html_file):
     ----------
     html_file : open file obj
     """
-    html = html_file.read()
+    html = open(html_file).read()
     html = html.decode('ascii', 'ignore')
-    soup = BeautifulSoup(html)
+    soup = bs4.BeautifulSoup(html)
     return {'text': soup.getText(), 'html': html}
-
 
 def parse_html_title(html_file):
     """
@@ -66,15 +106,14 @@ def parse_html_title(html_file):
     ----------
     html_file : open file obj
     """
-    html = html_file.read()
+    html = open(html_file).read()
     html = html.decode('ascii', 'ignore')
-    soup = BeautifulSoup(html)
+    soup = bs4.BeautifulSoup(html)
     title = ' '.join([item.getText() for item in soup.findAll('h3')])
     title = re.sub(r'(\d{4})(\d{4})', r'\1-\2', title)
     editors = '; '.join([item.getText() for item in soup.findAll('dd')])
     date = re.findall(r'(\d{4})\n', soup.getText())[0]
     return {'title': title, 'editors':editors, 'date':date}
-
 
 def parse_html_terms(html_file):
     """
@@ -84,9 +123,9 @@ def parse_html_terms(html_file):
     ----------
     html_file : open file obj
     """
-    html = html_file.read()
+    html = open(html_file).read()
     html = html.decode('ascii', 'ignore')
-    soup = BeautifulSoup(html)
+    soup = bs4.BeautifulSoup(html)
     terms_list = []
     for term in soup.findAll('li'):
         term_id =  term.find('span').attrs['id']
@@ -96,9 +135,143 @@ def parse_html_terms(html_file):
         terms_list.append({'id':term_id, 'acronym': acronym, 'def': defin})
     return terms_list
 
+def parse_html_refs(html_files, id_prefix=None):
+    """
+    Parses comp#.html from epub.
+
+    Parameters
+    ----------
+    html_files : list of file paths
+    id_prefix : str or None
+        prefix for document id, ex frus1958-60v04
+    """
+    if id_prefix is None:
+        id_prefix=''
+    ref_info = []
+    for html_file in html_files:
+        html = open(html_file).read()
+        html = html.decode('ascii', 'ignore')
+        ref_info += __parse_html_ref(html, id_prefix)
+    return ref_info
+ 
+def __parse_html_ref(html, id_prefix):
+    ###split up the html into ref sections since dates/notes are hard to 
+    ###extract
+    ref_info = []
+    html_list = html.split('<hr class="list"/>')
+    for html_item in html_list:
+        soup = bs4.BeautifulSoup(html_item)
+        try:
+            title = soup.findAll('h4')[0].getText().strip()
+            source = soup.findAll(
+                    'p', {'class':'sourcenote'})[0].getText().strip()
+            doc_num = re.findall(r'Document (\d{,4})', title)[0]
+            if not doc_num: continue 
+        except IndexError:
+            continue
+        try:
+            notes = re.findall(r'<p>\n\s+([\w,\.\s:]+)\n\s+</p>', 
+                    soup.prettify())[0]
+        except IndexError:
+            notes = None
+        ref_info.append({'id': '%sd%s'%(id_prefix, doc_num), 
+            'notes': notes, 'source': source})
+    return ref_info
+
+def parse_html_docs(html_files, full_names=None, id_prefix=None):
+    """
+    Parses d#.html from epub.
+
+    Parameters
+    ----------
+    html_files : list of file paths
+    full_names : set or list of strings
+        if not None will return a list of name_ids; otherwise will return
+        the name strings
+    id_prefix : str or None
+        prefix for document id, ex frus1958-60v04
+
+    Returns
+    -------
+    doc_info : dict if dicts
+    """
+    if id_prefix is None:
+        id_prefix=''
+    doc_info = {}
+    for html_file in html_files:
+        html = open(html_file).read()
+        html = html.decode('ascii', 'ignore')
+        parsed_doc = __parse_html_doc(html, full_names, id_prefix)
+        doc_info[parsed_doc['id']] = parsed_doc['doc']
+    return doc_info
+
+def __parse_html_doc(html, full_names, id_prefix):
+    soup = bs1.BeautifulSoup(html)
+    title = soup.title.getText()
+    ###get the doc id
+    doc_tag_attrs = soup.find('div', {'class':'document'}).attrs
+    if doc_tag_attrs:
+        doc_id = doc_tag_attrs[1][1]
+    else:
+        doc_id = 'XXX'
+    date_tag = soup.find('span', {'class': 'tei-date'})
+    if date_tag:
+        try:
+            date = dparser.parse(date_tag.getText())
+        except:
+            date = None
+        #drop the date from main body
+        date_tag.clear()
+    else:
+        date = None
+    footnotes_tag = soup.find('div', {'class':'footnotes'})
+    if footnotes_tag:
+        footnotes = footnotes_tag.getText()
+        ###drop the footnotes from main body
+        footnotes_tag.clear()
+    else:
+        footnotes = None
+    body = soup.getText()
+    names = __parse_html_person_codes(soup, full_names)
+    return {'id': id_prefix+doc_id, 'doc': 
+            {'title':title, 'date':date, 'footnotes': footnotes, 
+                'body': body, 'names':names}}
+
+def __parse_html_person_codes(soup, full_names):
+    """
+    Retieves all persons in docs, 
+    i.e. soup.findAll('span', {'class': 'tei-persName'}), matches these to the 
+    persons dictionary from parse_html_persons and returns the appropriate code.
+    """
+    names = [t.getText() for t in 
+            soup.findAll('span', {'class': 'tei-persName'})]
+    if full_names is None:
+        return names
+    else:
+        return __get_name_ids(names, full_names)
+
+def __get_name_ids(names, full_names, 
+        names_override={'eisenhower':'Eisenhower_Dwight_D.'}):
+    name_ids = set()
+    full_names = np.array(full_names)
+    for name in names:
+        name = name.lower()
+        name = name.split()
+        ###first check that name is not in  override names
+        if name[-1] in names_override.keys():
+            name_ids.add(names_override[name[-1]])
+            continue
+        ###try to find the best possible match
+        match_array = np.array(
+                [len([1 for part in name if part in n.lower()]) 
+                    for n in full_names])
+        best_match_index = match_array == match_array.max()
+        ##check that the match is unique
+        if sum(best_match_index)==1:
+            name_ids.add(full_names[best_match_index][0])
+    return list(name_ids)
+
 ###########################################################################
-
-
 
 def _get_tag_attrs(soup, tag='div'):
     """
@@ -135,7 +308,7 @@ def get_doc_sections(text, tag='div'):
     Takes raw (xml) text and returns a list of document sections. 
     """
     text = unidecode(text)
-    soup = BeautifulSoup(text, 'xml')
+    soup = bs4.BeautifulSoup(text, 'xml')
     return soup.findAll(tag)
 
 
@@ -239,19 +412,29 @@ if __name__=="__main__":
     DATA = os.getenv("DATA")
     FRUS = os.path.join(DATA, 'declass', 'frus')
     RAW = os.path.join(FRUS, 'raw')
-
+    import pdb; pdb.set_trace()
+    #parsed_vol = parse_epub_book(RAW, 'frus1958-60v04')
+    #parsed_vol = parse_epub_book(RAW, 'frus1958-60v10p1')
+    parsed_vol = parse_epub_book(RAW, 'frus1961-63v05')
     #frus1 = os.path.join(RAW, 'frus1969-76v01.xml')
 
-    #with open(frus1) as f:
+    #with frus1) as f:
     #    frus1_data = f.read()
     #parsed_xml = parse_xml(frus1_data)
-    persons = open(os.path.join(RAW, 'frus1958-60v04/OEBPS', 'persons.html'))
-    preface = open(os.path.join(RAW, 'frus1958-60v04/OEBPS', 'preface.html'))
-    title = open(os.path.join(RAW, 'frus1958-60v04/OEBPS', 'title.html'))
-    terms = open(os.path.join(RAW, 'frus1958-60v04/OEBPS', 'terms.html'))
-    
-    import pdb; pdb.set_trace()
-    parsed_persons = parse_html_persons(persons)
-    parsed_preface = parse_html_preface(preface)
-    parsed_title = parse_html_title(title)
-    parsed_terms = parse_html_terms(terms)
+#    persons = os.path.join(RAW, 'frus1958-60v04/OEBPS', 'persons.html')
+#    preface = os.path.join(RAW, 'frus1958-60v04/OEBPS', 'preface.html')
+#    title = os.path.join(RAW, 'frus1958-60v04/OEBPS', 'title.html')
+#    terms = os.path.join(RAW, 'frus1958-60v04/OEBPS', 'terms.html')
+#    comp = [os.path.join(RAW, 'frus1958-60v04/OEBPS', 'comp%s.html'%num)
+#            for num in range(1,8)]
+#    docs = [os.path.join(RAW, 'frus1958-60v04/OEBPS', 'd%s.html'%num)
+#            for num in range(1,351)]
+#
+#    parsed_persons = parse_html_persons(persons)
+#    parsed_preface = parse_html_preface(preface)
+#    parsed_title = parse_html_title(title)
+#    parsed_terms = parse_html_terms(terms)
+#    parsed_comp = parse_html_refs(comp, id_prefix='frus1958-60v04')
+#    import pdb; pdb.set_trace()
+#    parsed_docs = parse_html_docs(docs, parsed_persons.index, 
+#            id_prefix='frus1958-60v04')
