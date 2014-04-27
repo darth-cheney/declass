7376ed760c9a531bd1ef3e928f104627f5f8846e
dkrasner <dkrasner@math.columbia.edu>
2014-04-26 22:37:43 -0400

    db additions for frus helpers

--- declass/config/frus.sql
+++ declass/config/frus.sql
@@ -1,8 +1,8 @@
 /*Various mysql queries for the frus colletion DB*/
 
-/* the person table contains name information on individuals appearing in 
+/* the persons table contains name information on individuals appearing in 
 the FRUS docs*/
-create Table if not exists person(
+create Table if not exists persons(
     id VARCHAR(32),
     last VARCHAR(16),
     first VARCHAR(16),
@@ -30,31 +30,31 @@ create Table if not exists title(
     PRIMARY KEY (id)
 );
 
-/* term table contains all the terms which appear in the vols and their 
+/* terms table contains all the terms which appear in the vols and their 
 respective definitions*/
-create Table if not exists term(
+create Table if not exists terms(
     id VARCHAR(16),
     acronym VARCHAR(16),
-    definition TEXT,
+    def TEXT,
     PRIMARY KEY (id),
     KEY (acronym)
 );
 
-/* ref table contains all the references for each doc*/
-create Table if not exists ref(
+/* refs table contains all the references for each doc*/
+create Table if not exists refs(
     id VARCHAR(20),
     source TEXT,
     notes MEDIUMTEXT,
     PRIMARY KEY (id)
 );
 
-/* doc table contains all the docs and their respective info*/
-create Table if not exists doc(
+/* docs table contains all the docs and their respective info*/
+create Table if not exists docs(
     id VARCHAR(20),
     title TEXT,
     date datetime,
     footnotes MEDIUMTEXT,
-    body LONGTEXT
+    body LONGTEXT,
     PRIMARY KEY (id)
 );
 
@@ -63,7 +63,8 @@ create Table if not exists person_doc(
     pid VARCHAR(32),
     did VARCHAR(20),
     KEY (pid),
-    KEY (did)
+    KEY (did),
+    PRIMARY KEY (pid, did)
 );
 
 
